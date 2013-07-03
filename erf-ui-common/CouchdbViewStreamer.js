ffc73e476f6cc59f4cffb82ef8b7b9e389ea1a46
velezj <velezj@alum.mit.edu>
2013-07-03 19:12:57 -0400

    Added widget for batch-fetching couchdb view results and applying function forEach on them. Also added a Histogram widget for creating histograms of view data. Added such a histogram to the dashboard right panel jsut to see if it worked (and it does, with mouse pan and zoom)

---
+++ erf-ui-common/CouchdbViewStreamer.js
@@ -0,0 +1,70 @@
+
+// 
+// This is a module that represents the results for a couchdb view
+// functions.  It will stream the results, querying it and paging them in,
+// so that it lloks as if there is a singel continous stream by applying
+// the .forEach( fn ) to the items.
+
+define([
+  "dojo/store/CouchdbStore",
+  "dojo/_base/declare"],
+       
+function( CouchdbStore, 
+	  declare) {
+  
+
+  return declare( null, {
+
+    //
+    // Create a new couchdv view streamer. 
+    // THe given options are mixed in to this object.
+    // Must supply at least:
+    //    database_url : string
+    //    view : string
+    //    query : Object
+    constructor : function( options ) {
+      declare.safeMixin( this, options );
+      
+      // create the stoire
+      this.store = new CouchdbStore({
+	target: this.database,
+	view: this.view 
+      });
+     
+    },
+
+    
+    //
+    // The query to run against the view
+    query: undefined,
+    
+    //
+    // The batch size top fetch
+    batch_size: 100,
+
+    
+    //
+    // Iterate over all the elements from the view/query and call
+    // given function iwth them in order.
+    // This takes care of batching the couchdb requests under the hood
+    forEach: function( fn, offset ) {
+      
+      offset = offset || 0;
+      
+      // fetch the next batch from view
+      var q = this.query;
+      q.limit = this.batch_size;
+      q.skip = offset;
+      batch = this.store.query( q );
+      var self = this;
+      return batch.then( function( result ) {
+	if( result.length > 0 && offset < result.total ) {
+	  batch.forEach( fn );
+	  self.forEach( fn, offset + result.length );
+	}
+      });
+      
+    },
+    
+  });
+});
---
+++ erf-ui-common/Histogram.js
@@ -0,0 +1,128 @@
+
+//
+// A histogram based on the couchdb view
+
+
+define( [ 
+  "dojo/_base/declare",
+  "erf-ui-common/CouchdbViewStreamer",
+  "dijit/layout/ContentPane",
+  "dojox/charting/Chart",
+  "dojox/charting/plot2d/Bars",
+  "dojox/charting/plot2d/Lines",
+  "dojox/charting/plot2d/Columns",
+  "dojox/charting/axis2d/Default",
+  "dojox/charting/action2d/MouseZoomAndPan",
+  "dojox/charting/widget/Legend",
+  "dojox/charting/themes/Claro" ],
+
+function( declare,
+	  ViewStreamer,
+	  ContentPane,
+	  Chart,
+	  BarsPlot,
+	  LinesPlot,
+	  ColumnsPlot,
+	  DefaultAxes,
+	  MouseZoomAndPan,
+	  Legend,
+	  theme ) {
+
+
+  return declare( [ContentPane], {
+
+
+    //
+    // Must suppli at leaast:
+    //    min: number
+    //    max: number
+    //    database: url
+    //    view: url
+    //    query: object
+    //    series_title: string
+    constructor: function( options ) {
+      declare.safeMixin( this, options );
+      
+      this.view_streamer = new ViewStreamer({
+	database: this.database,
+	view: this.view,
+	query: this.query,
+	batch_size: this.batch_size
+      });
+    },
+
+    min: undefined,
+    
+    max: undefined,
+    
+    num_bins: 100,
+    
+    chart: undefined,
+    
+    view_streamer: undefined,
+    
+    postCreate: function() {
+      this.inherited( arguments );
+      
+      // create the bar chart widget
+      this.chart = new Chart( this.domNode );
+      this.chart.setTheme( theme );
+      this.chart.addPlot( "default", {
+	type: LinesPlot,
+	markers: true,
+	gap: 5
+      });
+      this.chart.addAxis( "x", { min: this.min, max: this.max } );
+      this.chart.addAxis( "y", {vertical: true} );
+
+      // create the zoom and pan
+      new MouseZoomAndPan( this.chart, "default", { axis: "x" } );
+
+      
+      // compute bins from the view and add them as a serier to chart
+      var hist_data = [];
+      var bins = [];
+      var step =  ( this.max - this.min ) / this.num_bins;
+      for( var i = this.min; i <= this.max; i += step ) {
+      
+	bins.push( { min: i,
+		     max: i + step,
+		     count: 0} );
+      }
+      
+      // bin the data from the view
+      var self = this;
+      this.view_streamer.forEach( function( row ) {
+	var item = row.value;
+	//console.log( "  stream item: " + dojo.toJson( item ) );
+	for( var i = 0; i < bins.length; ++i ) {
+	  var bin = bins[i];
+	  if( item >= bin.min && item < bin.max ) {
+	    bin.count += 1;
+	    return;
+	  }
+	}
+      }).then( function( res ) {
+	
+	//console.log( "BINS: " + dojo.toJson( bins ) );
+	
+	// convert form bins to histogram data (x,y)
+	for( var i = 0; i < bins.length; ++i ) {
+	  hist_data.push( { x: self.min + i * step,
+			    y: bins[i].count } );
+	}
+
+	//console.log( "HIST: " + dojo.toJson( hist_data) );
+	
+	// add teh data
+	self.chart.addSeries( self.series_title, hist_data );
+	
+	// render hte chart
+	self.chart.render();
+      });
+    },
+
+  });
+
+
+});
